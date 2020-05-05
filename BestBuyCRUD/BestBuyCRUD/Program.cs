44c1d0d94b423b447deaf0303080c925a7a5d1af
XXDOD456 <kylemcconnell94@gmail.com>
2020-05-05 12:51:52 -0500

    connection added

--- BestBuyCRUD/BestBuyCRUD/Program.cs
+++ BestBuyCRUD/BestBuyCRUD/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace BestBuyCRUD
 {
@@ -6,7 +9,57 @@ namespace BestBuyCRUD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var departments = GetAllDepartments();
+
+            foreach (var dept in departments)
+            {
+                Console.WriteLine(dept);
+            }
         }
+
+        static IEnumerable GetAllDepartments()
+        {
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = System.IO.File.ReadAllText("ConnectionString.txt");
+
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT Name FROM Departments;";
+
+            using (conn)
+            {
+                conn.Open();
+                List<string> allDepartments = new List<string>();
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read() == true)
+                {
+                    var currentDepartment = reader.GetString("Name");
+                    allDepartments.Add(currentDepartment);
+                }
+
+                return allDepartments;
+            }
+        }
+        static void CreateDepartment(string departmentName)
+        {
+            var connStr = System.IO.File.ReadAllText("ConnectionString.txt");
+
+            //If you adopt initializing the connection inside the using statement then you can't make a mistake 
+            //later when reorganizing or refactoring code and accidentally doing something that implicitly 
+            //opens a connection that isn't closed
+            using (var conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                MySqlCommand cmd = conn.CreateCommand();
+
+                // parameterized query to prevent SQL Injection
+                cmd.CommandText = "INSERT INTO departments (Name) VALUES (@departmentName);";
+                cmd.Parameters.AddWithValue("departmentName", departmentName);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
     }
 }
+
